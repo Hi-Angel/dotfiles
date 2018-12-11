94cf0f02373116fd3ecd829a1016a32fc7345470
Konstantin Kharlamov <Hi-Angel@yandex.ru>
2018-12-11 23:55:43 +0300

    Add a gdb command to grep from a command output
    Signed-off-by: Konstantin Kharlamov <Hi-Angel@yandex.ru>

--- gdb.py
+++ gdb.py
@@ -10,3 +10,21 @@
 #     return None
 
 # gdb.pretty_printers.append(lookup_type_my_int)
+
+class GrepCmd (gdb.Command):
+    """Execute command, but only show lines matching the pattern
+    Usage: grep_cmd <cmd> <pattern> """
+
+    def __init__ (_):
+        super ().__init__ ("grep_cmd", gdb.COMMAND_STATUS)
+
+    def invoke (_, args_raw, __):
+        args = gdb.string_to_argv(args_raw)
+        if len(args) != 2:
+            print("Wrong parameters number. Usage: grep_cmd <cmd> <pattern>")
+        else:
+            for line in gdb.execute(args[0], to_string=True).splitlines():
+                if args[1] in line:
+                    print(line)
+
+GrepCmd() # required to get it registered
