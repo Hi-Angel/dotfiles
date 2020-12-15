03f2186618cbe581a0cd44c3961fad8659111eb4
Konstantin Kharlamov <Hi-Angel@yandex.ru>
2020-12-15 18:34:11 +0300

    lisp: fix matching pair highlight with smartparens
    If action is not insert nor autoskip, just return true

--- .emacs.d/lisp/sp-sublimelike.el
+++ .emacs.d/lisp/sp-sublimelike.el
@@ -1,14 +1,14 @@
 (require 'smartparens)
 
-
 (defun sp-point-not-before-word (id action context)
   "In insert and autoskip actions returns t when next symbol is
 not a word constituent."
-  (when (or (eq action 'insert) (eq action 'autoskip))
-    (looking-at "\\(\\Sw\\|$\\)")))
+  (if (or (eq action 'insert) (eq action 'autoskip))
+      (looking-at "\\(\\Sw\\|$\\)")
+    t))
 
 (let ((when '(sp-point-not-before-word))
-      (actions  '(list insert wrap autoskip navigate)))
+      (actions  '(insert wrap autoskip navigate)))
   (sp-pair "{" "}" :when when :actions actions)
   (sp-pair "[" "]" :when when :actions actions)
   (sp-pair "(" ")" :when when :actions actions))
