1b73839bde8606fd808483598594302404c9876d
Konstantin Kharlamov <Hi-Angel@yandex.ru>
2022-12-11 00:25:30 +0300

    early-init.el: simplify a bunch of setq's

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,21 +1,18 @@
-;; only run garbage collection on idle
-(setq gc-cons-threshold (* 1024 1024 1024))
-(run-with-idle-timer 2 t (lambda () (garbage-collect)))
-
 ;;;; I set defaults in early-init rather than in .emacs to not waste CPU cycles
 ;;;; in excessive overwrites to them
 
-(setq frame-resize-pixelwise t) ;; needs to be set before a frame is created
-(setq kill-ring-max 1) ;; I never use more than one entry anyway
-(setq reb-re-syntax 'string) ;; standard syntax for re-builder
+;; only run garbage collection on idle
+(run-with-idle-timer 2 t (lambda () (garbage-collect)))
+(setq gc-cons-threshold (* 1024 1024 1024)
+      frame-resize-pixelwise t ;; needs to be set before a frame is created
+      kill-ring-max 1 ;; I never use more than one entry anyway
+      reb-re-syntax 'string ;; standard syntax for re-builder
 
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-(setq create-lockfiles nil)
-(setq native-compile-prune-cache t) ;; with native compilation, remove unused .eln's
-
-;; case sensitive dabbrev
-(setq dabbrev-upcase-means-case-search t)
+      make-backup-files nil
+      auto-save-default nil
+      create-lockfiles nil
+      native-compile-prune-cache t ;; with native compilation, remove unused .eln's
+      dabbrev-upcase-means-case-search t) ;; case sensitive dabbrev
 (setq-default case-fold-search nil)
 
 (setq-default display-line-numbers 'visual) ;; show the line numbers
